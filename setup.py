29ae266e48fbcc9f55c870b3ccf2d5f4da5cc68a
Garrett <garrettmay@live.de>
2021-04-14 15:38:51 +0200

    setup.py is ready

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(name='rescaleforvis',
       version='1.0',
       description='Maps each number of a short list to a whole number, such that the order of the magnitudes of the differences between any two numbers is preserved.',
-      url='',
+      url='https://github.com/garryFromGermany/rescale_for_vis',
       author='Garrett May',
       author_email='garrettmay@live.de',
       license='MIT',
